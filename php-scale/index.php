4a402b927567ca4c9acb6445e518665ab471265c
Student User <student@workstation.lab.example.com>
2020-09-06 17:51:21 +0000

    tdt

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,3 +1,3 @@
 <?php
-  print "This is version  of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+  print "This is version  of the app. I am running on host -> " TEST " " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
 ?>
